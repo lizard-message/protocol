b1f5d8d28fcc157c2fc5cae64c739f39ce40cbf4
phs <>
2020-09-22 18:14:22 +0800

    未完成客户端握手协议

--- src/send_to_server/decode.rs
+++ src/send_to_server/decode.rs
@@ -1,6 +1,19 @@
 use crate::state::ClientState;
 use bytes::BytesMut;
 use std::iter::Iterator;
+use std::convert::AsRef;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("parse error")]
+    Parse,
+}
+
+#[derive(Debug)]
+pub enum Message {
+
+}
 
 #[derive(Debug)]
 pub struct Decode {
@@ -8,7 +21,39 @@ pub struct Decode {
     state: Option<ClientState>,
 }
 
+impl Decode {
+    
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            buff: BytesMut::with_capacity(capacity),
+            state: None
+        }
+    }
+
+    pub fn get_mut_buff(&mut self) -> &BytesMut {
+        &mut self.buff
+    }
+
+    pub fn set_buff<R>(&mut self, buff: R) where R: AsRef<[u8]> {
+        self.buff.extend_from_slice(buff.as_ref())
+    }
+
+    pub fn iter(&mut self) -> Iter<'_> {
+        Iter {
+            source: self
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Iter<'a> {
     source: &'a mut Decode,
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = Result<Message, Error>;
+    
+    fn next(&mut self) -> Self::Item {
+    
+    }
 }
