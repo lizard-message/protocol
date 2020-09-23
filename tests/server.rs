884ec5a9ce3fca967d37e93ba997868e254ae607
phs <>
2020-09-23 17:59:53 +0800

    添加单元测试

---
+++ tests/server.rs
@@ -0,0 +1,99 @@
+use protocol::send_to_client::{
+    decode::{Decode, Message, Error},
+};
+use bytes::{BytesMut, BufMut};
+
+fn init(buff: &[u8]) -> Option<Result<Message, Error>> {
+    let mut decode = Decode::new(1024);
+
+    decode.set_buff(&buff);
+    decode.iter().next()
+}
+
+#[test]
+fn decode_hand_shake() {
+    let mut buff = BytesMut::new();
+
+    // hand shake
+    buff.put_u8(1);
+    
+    // version
+    buff.put_u8(1);
+
+    // mask
+    buff.put_u16_le(3);
+
+    // client message size
+    buff.put_u8(10);
+
+    // a success hand shake
+    if let Message::Info(version, mask, message_size) = init(&buff).unwrap().unwrap() {
+        assert_eq!(version, 1);
+        assert_eq!(mask, 3);
+        assert_eq!(message_size, 10);
+    }
+}
+
+#[test]
+#[should_panic]
+fn decode_hand_shake_error() {
+    let mut buff = BytesMut::new();
+
+    // hand shake
+    buff.put_u8(std::u8::MAX);
+    
+    // version
+    buff.put_u8(1);
+
+    // mask
+    buff.put_u16_le(3);
+
+    // client message size
+    buff.put_u8(10);
+
+    // a success hand shake
+    if let Message::Info(version, mask, message_size) = init(&buff).unwrap().unwrap() {
+        assert_eq!(version, 1);
+        assert_eq!(mask, 3);
+        assert_eq!(message_size, 10);
+    }
+}
+
+#[test]
+fn decode_hand_shake_chunk() {
+    let mut decode = Decode::new(1024);
+    let mut buff = BytesMut::new();
+
+    buff.put_u8(1);
+    decode.set_buff(&buff);
+    let result = decode.iter().next();
+    dbg!(&result);
+    assert!(result.is_none());
+
+    // buffer clear, so not value in buffer
+    buff.clear();
+    buff.put_u8(1);
+    decode.set_buff(&buff);
+    let result = decode.iter().next();
+    dbg!(&result);
+    assert!(result.is_none());
+
+    buff.clear();
+    buff.put_u16_le(3);
+    decode.set_buff(&buff);
+    let result = decode.iter().next();
+    dbg!(&result);
+    assert!(result.is_none());
+
+    buff.clear();
+    buff.put_u8(10);
+    decode.set_buff(&buff);
+    let result = decode.iter().next();
+    dbg!(&result);
+
+    if let Message::Info(version, mask, message_size) = result.unwrap().unwrap() {
+        assert_eq!(version, 1);
+        assert_eq!(mask, 3);
+        assert_eq!(message_size, 10);
+    }
+}
